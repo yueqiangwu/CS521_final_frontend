1644dfd5f166fca341f92bfcd1f160ddb221e7fe
yueqiangwu <wu147@illinois.edu>
2026-05-03 00:36:35 -0500

    Change base url

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { message } from "antd";
 
-const BASE_URL = "http://localhost:5000/api";
-// const BASE_URL = "https://cs521-code.onrender.com/api";
+// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://cs521-code.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
